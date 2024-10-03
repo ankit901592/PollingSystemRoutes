e642062738a8e126969f34e793b0346c998b05f5
ankit <ankitchakraborty@30@gmail.com>
2024-10-03 21:25:45 +0530

    updated command

--- server.js
+++ server.js
@@ -7,8 +7,20 @@ const Server = express();
 Server.use(bodyParser.urlencoded({ extended: true }));
 Server.use(bodyParser.json());
 
-Server.get("/", (req, res) => {
-  res.send("welcome to polling app");
+Server.get('/', (req, res) => {
+  res.send(`
+    <h1>Welcome to Polling App</h1>
+     <h1>Test this on postMan</h1>
+    <ul>
+      <li><a href="http://localhost:3000/api/questions/create">POST /api/questions/create</a></li>
+      <li><a href="http://localhost:3000/api/questions/:id/options/create">POST /api/questions/:id/options/create</a></li>
+      <li><a href="http://localhost:3000/api/questions/getAllQuestions">GET /api/questions/getAllQuestions</a></li>
+      <li><a href="http://localhost:3000/api/options/:id/add_vote">GET /api/options/:id/add_vote</a></li>
+      <li><a href="http://localhost:3000/api/questions/:id/delete">DELETE /api/questions/:id/delete</a></li>
+      <li><a href="http://localhost:3000/api/options/:id/delete">DELETE /api/options/:id/delete</a></li>
+      <li><a href="http://localhost:3000/api/questions/:id">GET /api/questions/:id</a></li>
+    </ul>
+  `);
 });
 Server.use("/api", Questionroutes);
 
